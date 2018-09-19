1e8ef3c90bfa823d39024d4bf7f4595593fd4b2d
unknown <aadhil.imam@gmail.com>
2018-09-19 10:26:33 -0700

    make it better

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import _ from 'lodash';
+import React , {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+
+
+
+
+
+const API_KEY = 'AIzaSyDXbWXEf47mWnwy_pPDxq11SF_MEFfcj_g';
+
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+             videos : [],
+             selectedVideo : null
+            };
+
+       this.videoSearch('Sri Lanka');
+      
+     }
+
+     videoSearch(term){
+        YTSearch({key:API_KEY , term:term} , (videos) => {
+            this.setState({
+                videos : videos,
+                selectedVideo:videos[0]
+         
+            });
+            });
+
+     }
+
+    render(){
+
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)} , 300);
+    return( 
+        <div>
+          <h1>Youtube Clone</h1>  
+             <SearchBar onSearchTermChange={videoSearch}/>
+             <VideoDetail video={this.state.selectedVideo}/> 
+             <VideoList
+              onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+              videos={this.state.videos}/>
+        </div>
+    );
+    }  
+}
+
+
+ReactDOM.render(<App/> , document.querySelector('.container'));
+
